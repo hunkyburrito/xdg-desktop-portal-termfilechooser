86d1bd37eba89ba0b524fadfafd9d5e2d3940224
cyberduck <thecyberduck@tutanota.com>
2024-05-22 00:21:19 +0530

    feat: improve vifm wrapper script

--- contrib/vifm-wrapper.sh
+++ contrib/vifm-wrapper.sh
@@ -57,37 +57,39 @@ fi
 
 if [ "$save" = "1" ]; then
     # Save/download file
-    set -- --choose-files "$out" -c "set statusline='Select save path (see tutorial in preview pane; try pressing <w> key if no preview)'" --select "$path"
-    printf '%s' 'xdg-desktop-portal-termfilechooser saving files tutorial
+    set -- --choose-files "$out" -c "view" -c "map <esc> :cquit<cr>" -c "set statusline='Select save path (see tutorial in preview pane; try pressing <w> key if no preview)'" --select "$path"
+	printf '%b' "
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!            === \033[1mWARNING!\033[0m ===              !!!
+!!! The contents of *\033[1mwhatever\033[0m* file you open !!!
+!!! last in vifm, will be ***\033[1moverwritten\033[0m***! !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+>>>>>>> 5ad2dfc (improve vifm wrapper script)
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!                 === WARNING! ===                 !!!
-!!! The contents of *whatever* file you open last in !!!
-!!! ranger will be *overwritten*!                    !!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+\033[1mNotice:\033[0m
+1) The file under \033[1mcursor\033[0m is provided for your
+   convenience. You can delete it and choose
+   another file to overwrite that file.
+2) If you \033[1m:quit\033[0m vifm, this file will be removed
+   and the save operation will be aborted.
+3) However, if you abort save operation after
+   renaming the file, it won't be removed.
 
-Instructions:
+\033[1mInstructions\033[0m:
 1) Move this file wherever you want.
-2) Rename the file if needed.
-3) Confirm your selection by opening the file, for
-   example by pressing <Enter>.
-
-Notes:
-1) This file is provided for your convenience. You
-   could delete it and choose another file to overwrite
-   that, for example.
-2) If you quit ranger without opening a file, this file
-   will be removed and the save operation aborted.
-' >"$path"
+2) Rename this file as necessary.
+3) Confirm selection by pressing <Enter>.
+4) To cancel the save operation press <Esc>.
+" >"$path"
 elif [ "$directory" = "1" ]; then
     # upload files from a directory
-    set -- --choose-dir "$out" -c "set statusline='Select directory (quit in dir to select it)'"
+	set -- --choose-dir "$out" -c "only" -c "map <esc> :cquit<cr>" -c "set statusline='Select directory (quit in dir to select it, press <Esc> to cancel)'"
 elif [ "$multiple" = "1" ]; then
     # upload multiple files
-    set -- --choose-files "$out" -c "set statusline='Select file(s) (press <t> key to select multiple)'"
+	set -- --choose-files "$out" -c "only" -c "map <esc> :cquit<cr>" -c "set statusline='Select file(s) (press <t> key to select multiple, press <Esc> to cancel)'"
 else
     # upload only 1 file
-    set -- --choose-files "$out" -c "set statusline='Select file (open file to select it)'"
+	set -- --choose-files "$out" -c "only" -c "map <esc> :cquit<cr>" -c "set statusline='Select file (open file to select it, press <Esc> to cancel)'"
 fi
 $termcmd -- $cmd "$@"
 
