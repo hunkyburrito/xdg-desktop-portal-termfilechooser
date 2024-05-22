fc644c1c3c00c9eb29a2038cb5dd9c8c70b0690f
cyberduck <thecyberduck@tutanota.com>
2024-05-22 14:21:13 +0530

    fix: updated vifm-wrapper.sh

--- contrib/vifm-wrapper.sh
+++ contrib/vifm-wrapper.sh
@@ -57,33 +57,12 @@ fi
 
 if [ "$save" = "1" ]; then
     # Save/download file
-    set -- --choose-files "$out" -c "view" -c "map <esc> :cquit<cr>" -c "set statusline='Select save path (see tutorial in preview pane; try pressing <w> key if no preview)'" --select "$path"
-	printf '%b' "
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!            === \033[1mWARNING!\033[0m ===              !!!
-!!! The contents of *\033[1mwhatever\033[0m* file you open !!!
-!!! last in vifm, will be ***\033[1moverwritten\033[0m***! !!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
->>>>>>> 5ad2dfc (improve vifm wrapper script)
-
-\033[1mNotice:\033[0m
-1) The file under \033[1mcursor\033[0m is provided for your
-   convenience. You can delete it and choose
-   another file to overwrite that file.
-2) If you \033[1m:quit\033[0m vifm, this file will be removed
-   and the save operation will be aborted.
-3) However, if you abort save operation after
-   renaming the file, it won't be removed.
-
-\033[1mInstructions\033[0m:
-1) Move this file wherever you want.
-2) Rename this file as necessary.
-3) Confirm selection by pressing <Enter>.
-4) To cancel the save operation press <Esc>.
-" >"$path"
+	set -- --choose-files "$out" -c "only" -c "map <esc> :cquit<cr>" \
+		-c "set statusline='Save file (press <Enter> to select or <Esc> to cancel)%NCursorfile is the recommended choice, you can rename/move it%NIf you select another file, it will be overwritten by the save'" \
+		--select "$path" >"$path"
 elif [ "$directory" = "1" ]; then
     # upload files from a directory
-	set -- --choose-dir "$out" -c "only" -c "map <esc> :cquit<cr>" -c "set statusline='Select directory (quit in dir to select it, press <Esc> to cancel)'"
+	set -- --choose-dir "$out" -c "only" -c "map <esc> :cquit<cr>" -c "set statusline='Select directory (:quit in dir to select it, press <Esc> to cancel)'"
 elif [ "$multiple" = "1" ]; then
     # upload multiple files
 	set -- --choose-files "$out" -c "only" -c "map <esc> :cquit<cr>" -c "set statusline='Select file(s) (press <t> key to select multiple, press <Esc> to cancel)'"
