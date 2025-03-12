a318e95d6f0a0e3b8d29d238e4b7799f0343f7f8
hunkyburrito <hunkyburrito@protonmail.com>
2025-03-12 18:41:09 -0500

    refactor nnn wrapper support both quit and open style selection for directories add last_selected support fix shebang

--- contrib/nnn-wrapper.sh
+++ contrib/nnn-wrapper.sh
@@ -1,4 +1,4 @@
-#!/bin/sh bash
+#!/bin/sh
 
 set -x
 # This wrapper script is invoked by xdg-desktop-portal-termfilechooser.
@@ -10,9 +10,8 @@ directory="$2"
 save="$3"
 path="$4"
 out="$5"
-# termcmd="x-terminal-emulator -e"
 termcmd="${TERMCMD:-kitty --title 'termfilechooser'}"
-cmd="nnn -S -s xdg-portal-filechooser"
+cmd="nnn -S -s termfilechooser"
 # -S [-s <session_file_name>] saves the last visited dir location and opens it on next startup
 
 # See also: https://github.com/jarun/nnn/wiki/Basic-use-cases#file-picker
@@ -27,48 +26,54 @@ cmd="nnn -S -s xdg-portal-filechooser"
 # Although perhaps it could be scripted together with https://github.com/jarun/nnn/wiki/Basic-use-cases#configure-cd-on-quit
 # This missing functionality probably could be implemented with a plugin.
 
+last_selected_path="${XDG_STATE_HOME:-$HOME/.local/state}/xdg-desktop-portal-termfilechooser/last_selected"
+mkdir -p "$(dirname "$last_selected_path")"
+if [ ! -f "$last_selected_path" ]; then
+    touch "$last_selected_path"
+fi
+last_selected="$(cat "$last_selected_path")"
+
+# Restore last selected path
+if [ -d "$last_selected" ]; then
+    save_to_file=""
+    if [ "$save" = "1" ]; then
+        save_to_file="$(basename "$path")"
+        path="${last_selected}/${save_to_file}"
+    else
+        path="${last_selected}"
+    fi
+fi
+
 if [ -z "$path" ]; then
     path="$HOME"
 fi
 
-create_save_file() {
-    cat >"$path" <<-'END'
-	xdg-desktop-portal-termfilechooser saving files tutorial
-
-	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	!!!                 === WARNING! ===                 !!!
-	!!! The contents of *whatever* file you open last in !!!
-	!!! nnn will be *overwritten*!                       !!!
-	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-	Instructions:
-	1) Move this file wherever you want.
-	2) Rename the file if needed.
-	3) Confirm your selection by opening the file, for
-		 example by pressing <Enter>.
-
-	Notes:
-	1) This file is provided for your convenience. You
-		 could delete it and choose another file to overwrite
-		 that, for example.
-	2) If you quit nnn without opening a file, this file
-     will be removed and the save operation aborted.
-	END
-    rm "$out" || : # Remove the old /tmp/termfilechooser.portal
-    cleanup() {
-        # Remove the file with the above tutorial text if the calling program did not overwrite it.
-        if [ ! -s "$out" ] || [ "$path" != "$(cat "$out")" ]; then
-            rm "$path" || :
-        fi
-    }
-    trap cleanup EXIT HUP INT QUIT ABRT TERM
-}
-
 if [ "$save" = "1" ]; then
-    create_save_file
+    # Save tutorial
+    printf "%s" 'xdg-desktop-portal-termfilechooser saving files tutorial
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!                 === WARNING! ===                 !!!
+!!! The contents of *whatever* file you open last in !!!
+!!! nnn will be *overwritten*!                       !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+Instructions:
+1) Move this file wherever you want.
+2) Rename the file if needed.
+3) Confirm your selection by opening the file, for
+	 example by pressing <Enter>.
+
+Notes:
+1) This file is provided for your convenience. You
+	 could delete it and choose another file to overwrite
+	 that, for example.
+2) If you quit nnn without opening a file, this file
+     will be removed and the save operation aborted.
+' >"$path"
     set -- -p "$out" "$path"
 else
-    set -- -p "$out"
+    set -- -p "$out" "$path"
 fi
 
 command="$termcmd $cmd"
@@ -79,17 +84,29 @@ for arg in "$@"; do
     command="$command \"$escaped\""
 done
 
-# data will be `cd "/dir/path"`
-NNN_TMPFILE_PATH="/tmp/xdg-desktop-portal-termfilechooser-NNN-tmpfile"
-if [ "$directory" -eq "1" ]; then
-    sh -c "env NNN_TMPFILE=\"$NNN_TMPFILE_PATH\" $command"
+if [ "$directory" = "1" ]; then
+    sh -c "env NNN_TMPFILE=\"$out\" $command"
 else
     sh -c "$command"
 fi
 
-if [ "$directory" -eq "1" ] && [ -n "$NNN_TMPFILE_PATH" ]; then
-    LAST_SELECTED_DIR=$(cat "$NNN_TMPFILE_PATH")
-    # convert data from `cd "/dir/path"`  to "/dir/path"
-    LAST_SELECTED_DIR=$(echo "$LAST_SELECTED_DIR" | sed -n "s/^cd '\(.*\)'/\1/p")
-    echo "$LAST_SELECTED_DIR" >"$out"
+NNN_QUIT=0
+if [ "$save" = "0" ] && [ "$directory" = "1" ] && [ -s "$out" ]; then
+    # select on quit; file data will be `cd '/dir/path'`
+    if [ "$(cut -c -2 "$out")" = "cd" ]; then
+        sed -i "s/^cd '\(.*\)'/\1/" "$out"
+        NNN_QUIT=1
+    fi
+fi
+
+# Remove the file with the above tutorial text if the calling program did not overwrite it.
+if [ "$save" = "1" ] && [ ! -s "$out" ] || [ "$path" != "$(cat "$out")" ]; then
+    rm "$path"
+else
+    selected_path="$(tail -n 1 "$out")"
+    if [ "$NNN_QUIT" = "1" ]; then
+        echo "$selected_path" >"$last_selected_path"
+    else
+        dirname "$selected_path" >"$last_selected_path"
+    fi
 fi
